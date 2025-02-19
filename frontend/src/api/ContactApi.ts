d8618340cd046620a18fabd810c7b343dd6dac23
vrabelmarian <mrn.vrabel@gmail.com>
2025-02-19 17:48:45 +0100

    feat: axios api calls

--- frontend/src/api/ContactApi.ts
+++ frontend/src/api/ContactApi.ts
@@ -1,7 +1,24 @@
 import axios from 'axios'
+import { Contact } from '../data'
 
 const API_URL = 'http://localhost:8080/contacts'
 
 export async function getContacts(page = 0, size = 10) {
   return await axios.get(`${API_URL}?page=${page}&size=${size}`)
+}
+
+export async function saveContact(contact: Contact) {
+  return await axios.post(API_URL, contact)
+}
+
+export async function getContact(id: string) {
+  return await axios.get(`${API_URL}/${id}`)
+}
+
+export async function updateContact(contact: Contact) {
+  return await axios.post(API_URL, contact)
+}
+
+export async function deleteContact(id: string) {
+  return await axios.delete(`${API_URL}/${id}`)
 }
